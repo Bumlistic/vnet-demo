360c1b718f2db66778a86a264235584a731e1252
Obumneke Onwumelu <bumlistic@yahoo.com>
2025-10-02 11:04:50 -0700

    updated provider

--- provider.tf
+++ provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "4.41.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "vnet1-storage-infra-rg"
+    storage_account_name = "vnet1demostorageaccount"
+    container_name = "vnet1-container"
+    key = "vnet1.tfstate"
+    subscription_id = "b65e8ffb-04e2-41cf-a206-4af301cfe106"
+    use_azuread_auth     = true
+  }
 }
 
 provider "azurerm" {
